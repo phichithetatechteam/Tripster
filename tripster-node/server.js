c8c549046f2990c1c860a410a47ff97e775312ed
tdonati <tdonati@bu.edu>
2019-03-21 23:22:09 -0400

    Got server running

--- tripster-node/server.js
+++ tripster-node/server.js
@@ -10,30 +10,11 @@ const databaseConnection = JSON.parse(fs.readFileSync('./mongo_settings.json', '
 const app = express()
 
 // start express server
-const server = app.listen(3000, () => console.log('Listening on Port 3000'))
+const server = app.listen(8888, () => console.log('Listening on Port 8888'))
 module.exports = server
 
-// Creates the Profile entity
-app.post('/profile/new', function (req, res) {
-  const response = dbFunctions.post_new_profile_entity(mongoClient, databaseConnection.dbURL, databaseConnection.dbName,
-    databaseConnection.collectionName, req.query.username,
-    req.query.first_name, req.query.last_name, req.query.age)
-  response.then(function (resp) {
-    res.status(resp.status)
-    res.send(resp.message)
-  }).catch(function (error) {
-    res.send(error)
-  })
-})
-
 // Retrieves username, first name, last name, age if the user exists in the data source.
-app.get('/profile/:id', function (req, res) {
-  const response = dbFunctions.retrieve_data_by_id(mongoClient, databaseConnection.dbURL, databaseConnection.dbName,
-    databaseConnection.collectionName, req.params.id)
-  response.then(function (resp) {
-    res.status(resp.status)
-    res.send(resp.message)
-  }).catch(function (error) {
-    res.send(error)
-  })
+app.get('/get-route', function (req, res) {
+
+  res.send("SERVER IS RUNNING")
 })
