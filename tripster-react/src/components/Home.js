8b55ffde017c1b9dab9e899a79b8382fa8431a22
Jin Young Bang <jybang@bu.edu>
2019-04-13 11:58:33 -0400

    facebook api

--- tripster-react/src/components/Home.js
+++ tripster-react/src/components/Home.js
@@ -1,20 +1,59 @@
 import React from "react";
+import FacebookLogin from 'react-facebook-login';
 import {Button, Card} from "antd";
 import { withRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 export class Home extends React.Component {
+
+    state = {
+        isLoggedIn: false,
+        userID: '',
+        name: '',
+        email: '',
+        picture: ''
+    }
+
+    responseFacebook = response => {
+        // console.log(response);
+
+        this.setState({
+            isLoggedIn: true,
+            userID: response.userID,
+            name: response.name,
+            email: response.email,
+            picture: response.picture.data.url
+        });
+    }
+
+    componentClicked = () => console.log("clicked");
+
     render() {
+
+        let fbContent;
+
+        if (this.state.isLoggedIn) {
+            // return <Redirect to='/trips' />
+
+            this.props.history.push('/trips');
+        }
+
+        else {
+            fbContent = (<FacebookLogin
+                appId="242487039919018"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} />);
+        }
+
         return (
             <div>
-                <Card>
-                    <Button
-                        type="primary"
-                        onClick={() => console.log("Do login with facebook functionality")}
-                    >Login With Facebook
-                    </Button>
-                </Card>
+                <h1>Welcome to Tripster</h1>
+                {fbContent}
 
             </div>
+
         );
     }
 }
