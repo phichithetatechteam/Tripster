580b799b8e74cc93a0bd670aa6e9c1a34b0e7aaa
Jin Young Bang <jybang@bu.edu>
2019-04-26 20:16:22 -0400

    changed backend url

--- tripster-react/src/dependency.js
+++ tripster-react/src/dependency.js
@@ -1,2 +1,3 @@
-var backendURL = "http://localhost:8888";
+// var backendURL = "http://localhost:8888";
+var backendURL = "http://ec2-3-14-1-163.us-east-2.compute.amazonaws.com:8888";
 export {backendURL}
