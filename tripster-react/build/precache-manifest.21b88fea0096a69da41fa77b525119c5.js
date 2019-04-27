20857132940bf7bc85eaceff2ac71856043dca23
Jin Young Bang <jybang@bu.edu>
2019-04-26 20:24:36 -0400

    changed backend point

---
+++ tripster-react/build/precache-manifest.21b88fea0096a69da41fa77b525119c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9835fbc26fabdec24482",
+    "url": "/static/js/main.6fb04278.chunk.js"
+  },
+  {
+    "revision": "1044736ae1e68b77dbf1",
+    "url": "/static/js/2.8dd06114.chunk.js"
+  },
+  {
+    "revision": "9835fbc26fabdec24482",
+    "url": "/static/css/main.932c09be.chunk.css"
+  },
+  {
+    "revision": "1044736ae1e68b77dbf1",
+    "url": "/static/css/2.c8d7d069.chunk.css"
+  },
+  {
+    "revision": "f79378a1c3e7c79fdbda1febb36ada3d",
+    "url": "/index.html"
+  }
+];
